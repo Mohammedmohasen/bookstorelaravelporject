40aa2034015bd88004ed6a4d1dc1da44b8655323
mohammed <mohmohsen2003@gmail.com>
2021-06-28 16:02:15 +0300

    edit rooute desighn

--- resources/views/admin/admin-edit-publishing-home.blade.php
+++ resources/views/admin/admin-edit-publishing-home.blade.php
@@ -46,31 +46,7 @@
                </div>
             </div>
             <div id="sidebar-scrollbar">
-               <nav class="iq-sidebar-menu">
-                  <ul id="iq-sidebar-toggle" class="iq-menu">
-                     <li>
-                        <a href="#dashboard" class="iq-waves-effect" data-toggle="collapse" aria-expanded="false"><span class="ripple rippleEffect"></span><i class="las la-home iq-arrow-left"></i><span>Shop</span></i></a>
-                        <ul id="dashboard" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
-                                 <li><a href="{{ route('book') }}"></i>BOOKS</a></li>
-                           <li><a href="category.html">Category</a></li>
-                           <li><a href="book-page.html">AUTHER</a></li>
-                           <li><a href="book-pdf.html">publishing house</a></li>
-                           <li><a href="Checkout.html">COMMETNT</a></li>
-
-                        </ul>
-                     </li>
-                     <li class="active active-menu">
-                        <a href="#admin" class="iq-waves-effect" data-toggle="collapse" aria-expanded="true"><span class="ripple rippleEffect"></span><i class="las la-home iq-arrow-left"></i><span>Admin</span></i></a>
-                        <ul id="admin" class="iq-submenu collapse show" data-parent="#iq-sidebar-toggle">
-                             <li class="active"><a href="{{ route('add-publishing-house') }}">ADD publishing house</a></li>
-                           <li><a href="{{ route('add-category')}}">ADD Books Category</a></li>
-                           <li "><a href="{{ route('add-author')}}">ADD Author</a></li>
-                           <li><a href="{{ route('add-BOOKS')}}">ADD Books</a></li>
-
-                        </ul>
-                     </li>
-                  </ul>
-               </nav>
+               @include('include.list')
 
             </div>
          </div>
---
+++ resources/views/include/list.blade.php
@@ -0,0 +1,25 @@
+<nav class="iq-sidebar-menu">
+                  <ul id="iq-sidebar-toggle" class="iq-menu">
+                     <li>
+                        <a href="#dashboard" class="iq-waves-effect" data-toggle="collapse" aria-expanded="false"><span class="ripple rippleEffect"></span><i class="las la-home iq-arrow-left"></i><span>Shop</span></i></a>
+                        <ul id="dashboard" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
+                           <li><a href="{{ route('book') }}"></i>BOOKS</a></li>
+                           <li><a href="{{ route('category') }}">Category</a></li>
+                           <li><a href="{{ route('author') }}">AUTHER</a></li>
+                           <li><a href="{{ route('publishing') }}">publishing house</a></li>
+                         
+
+                        </ul>
+                     </li>
+                     <li >
+                        <a href="#admin" class="iq-waves-effect" data-toggle="collapse" aria-expanded="true"><span class="ripple rippleEffect"></span><i class="las la-home iq-arrow-left"></i><span>Admin</span></i></a>
+                        <ul id="admin" class="iq-submenu collapse show" data-parent="#iq-sidebar-toggle">
+                           <li><a href="{{ route('add-publishing-house') }}">ADD publishing house</a></li>
+                           <li><a href="{{ route('add-category')}}">ADD Books Category</a></li>
+                           <li><a href="{{ route('add-author')}}">ADD Author</a></li>
+                           <li><a href="{{ route('add-BOOKS')}}">ADD Books</a></li>
+
+                        </ul>
+                     </li>
+                  </ul>
+               
